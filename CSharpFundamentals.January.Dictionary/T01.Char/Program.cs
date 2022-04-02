2e449e65690e3550f87041478949419ae8e104f6
Ceco ceco <tsvetomirpch78@gmail.com>
2022-04-02 12:10:28 +0300

    Added Lab Task 1 and description

---
+++ CSharpFundamentals.January.Dictionary/T01.Char/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> occurances = new SortedDictionary<int, int>();
+            foreach (int number in numbers)
+            {
+                if (!occurances.Any(x => x.Key == number))
+                {
+                    occurances.Add(number, 1);
+                }
+                else
+                {
+                    occurances[number]++;
+                }
+            }
+
+            foreach (var number in occurances)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
