beb4c2e035c5dd5f736a8f4d0d1a4c13453cc7c5
Ceco ceco <tsvetomirpch78@gmail.com>
2022-04-02 12:15:06 +0300

    Added T2 from lab + no description

---
+++ CSharpFundamentals.January.Dictionary/T02. Odd Occurrences/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02.Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> count = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                string currWord = word.ToLower();
+                if (!count.ContainsKey(currWord))
+                {
+                    count.Add(currWord, 1);
+                }
+                else
+                {
+                    count[currWord]++;
+                }
+            }
+            string[] sorted = count.Where(x => x.Value % 2 != 0).Select(x => x.Key).ToArray();
+            foreach (string word in sorted)
+            {
+                Console.Write(word + " ");
+            }
+        }
+    }
+}
